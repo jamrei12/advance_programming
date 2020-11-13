571b0aa094fad494a83757b1b3a84dda88c4a16f
jamrei12 <jamreimanalo@gmail.com>
2020-11-13 21:29:55 +0800

    Class and Functions

--- index.php
+++ index.php
@@ -1,100 +1,57 @@
-<!DOCTYPE html>
 <?
   // Creator : Jamrei M. Manalo
-  // Date Created : November 1, 2020
+  // Date Created : November 4, 2020
   // Subject : Advance Programming
   // Professor : Ms. Blances Sanchez
 ?>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <style media="screen">
-    h1{
-      position: relative; left:70px; top:150px;
-      color: black;
-    }
-    #form1{
-      position: relative; left: 500px;
-    }
-    #form2{
-      position: relative; left: 800px; top:-290px;
-    }
-    </style>
-  </head>
-  <body>
-    <h1>Jamrei M. Manalo</h1>
-    <form class="form" id="form1" method="post">
-      <h2>Calculator</h2>
-      Input the first number<br><br>
-      <input type="text" name="num01" id="num01" >
-      <br><br>Input the Second number<br><br>
-      <input type="text" name="num02" id="num02" ><br><br>
-      <input type="submit" name="add" value="+">
-      <input type="submit" name="minus" value="-">
-      <input type="submit" name="multiply" value="*">
-      <input type="submit" name="divide" value="/">
-      <br><br>Answer
-      <textarea name="name" onfocus="this.value=''" rows="1" cols="20">
-        <?php
-        if(isset($_POST['add']))
-        {
-            $num01 = $_POST['num01'];
-            $num02 = $_POST['num02'];
-            $add = $num01 + $num02;
-            echo $add;
+<?php
+        class User{
+          //Properties
+          public $nameKo;
+          public $usernameKo;
+          public $emailKo;
+          //Methods
+          /*function set_name($nameKo){
+              $this->name=$nameKo;
+          }
+          function get_name(){
+            return $this->name;
+          }
+          function set_username($usernameKo){
+            $this->username=$usernameKo;
+          }
+          function get_username(){
+            return $this->username;
+          }
+          function set_email($emailKo){
+            $this->email=$emailKo;
+          }
+          function get_email(){
+            return $this->email;
+          }*/
+          function addFriend($nameKo){
+            echo "$nameKo" . " added a friend. <br>";
+          }
+          function postStatus($nameKo){
+            echo "$nameKo" . " posted a status. <br>";
+          }
         }
-        if(isset($_POST['minus']))
-        {
-            $num01 = $_POST['num01'];
-            $num02 = $_POST['num02'];
-            $minus = $num01 - $num02;
-            echo $minus;
-        }
-        if(isset($_POST['multiply']))
-        {
-            $num01 = $_POST['num01'];
-            $num02 = $_POST['num02'];
-            $multiply = $num01 * $num02;
-            echo $multiply;
-        }
-        if(isset($_POST['divide']))
-        {
-            $num01 = $_POST['num01'];
-            $num02 = $_POST['num02'];
-            $divide = $num01 / $num02;
-            echo $divide;
-        }
-         ?>
+        $example = new User();
+        /*$example -> set_name('Jamrei M. Manalo');
+        $example -> set_username('jamrei12');
+        $example -> set_email('jamreimanalo@gmail.com');
+        echo "<br>";
+        echo"Name : " . $example->get_name();
+        echo "<br>";
+        echo "Username : ". $example->get_username();
+        echo "<br>";
+        echo "Email Address   : ". $example->get_email();
+        echo "<br><br>";*/
+        echo $example->addFriend('Jamrei M. Manalo');
+        echo "<br>";
+        echo $example->postStatus('Jamrei M. Manalo');
 
-      </textarea>
-    </form>
-    <form action="#" id="form2" method="post">
-      <h2>Getting the GCD</h2>
-      Input the first integer<br><br>
-      <input type="text" name="number1">
-      <br><br>Input the second integer<br><br>
-      <input type="text" name="number2" ><br><br>
-      <input type="submit" name="gcd" value="Submit">
-      <br><br>Answer
-      <textarea name="name1" onfocus="this.value=''" rows="1" cols="40">
-        <?php
-        if(isset($_POST['gcd'])){
-            $number1 = $_POST['number1'];
-            $number2 = $_POST['number2'];
-            $gcd = $_POST['gcd'];
-            if($number1 > $number2){
-                $temp = $number1;
-                $number1 = $number2;
-                $number2 = $temp;
-            }
-            for($i = 1; $i < ($number1+1); $i++ ){
-                if ($number1%$i == 0 and $number2%$i == 0)
-                   $gcd = $i;
-            }
-            echo $gcd;
-        }
-         ?>
-      </textarea>
-    </form>
-  </body>
-</html>
+
+
+
+  ?>
